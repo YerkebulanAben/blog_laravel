131208a68036ac82bcee5548b0dedb5d49dbc96e
Yerkebulan Aben <yerkebulan.aben@gmail.com>
2021-05-24 11:45:17 +0600

    Restrict access to an admin panel

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::prefix('admin')->name('admin.')->group(function (){
+Route::prefix('admin')->name('admin.')->middleware('admin')->group(function (){
    Route::get('/', [MainController::class, 'index'])->name('index');
    Route::resource('/categories', CategoryController::class);
    Route::resource('/tags', TagController::class);
